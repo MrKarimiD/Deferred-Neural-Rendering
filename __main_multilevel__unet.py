ab0e9bf939d1a3c399c310948ce92f5517611c65
MrKarimiD <KN2C90soft>
2020-05-19 23:00:38 -0400

    Add original U-Net as generator

--- __main_multilevel__unet.py
+++ __main_multilevel__unet.py
@@ -4,6 +4,7 @@ from renderer import *
 from framebuffer import *
 from random import randint
 import argparse
+from unet import UNet
 
 
 if __name__ == '__main__':
@@ -28,59 +29,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    class Net(nn.Module):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.conv1 = nn.Conv2d(16, 64, kernel_size=4, stride=2)
-            self.conv1_in = nn.InstanceNorm2d(64)
-
-            self.conv2 = nn.Conv2d(64, 128, kernel_size=4, stride=2)
-            self.conv2_in = nn.InstanceNorm2d(128)
-
-            self.conv3 = nn.Conv2d(128, 256, kernel_size=4, stride=2)
-            self.conv3_in = nn.InstanceNorm2d(256)
-
-            self.conv4 = nn.Conv2d(256, 512, kernel_size=4, stride=2)
-            self.conv4_in = nn.InstanceNorm2d(512)
-
-            self.conv5 = nn.Conv2d(512, 512, kernel_size=4, stride=2)
-            self.conv5_in = nn.InstanceNorm2d(512)
-
-            self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2)
-            self.deconv1_in = nn.InstanceNorm2d(512)
-
-            self.deconv2 = nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2)
-            self.deconv2_in = nn.InstanceNorm2d(512)
-
-            self.deconv3 = nn.ConvTranspose2d(768, 256, kernel_size=4, stride=2)
-            self.deconv3_in = nn.InstanceNorm2d(256)
-
-            self.deconv4 = nn.ConvTranspose2d(384, 128, kernel_size=4, stride=2, output_padding=1)
-            self.deconv4_in = nn.InstanceNorm2d(128)
-
-            self.deconv5 = nn.ConvTranspose2d(192, 3, kernel_size=4, stride=2)
-            self.deconv5_in = nn.InstanceNorm2d(3)
-
-        def forward(self, x):
-            # Encoder
-            x = F.leaky_relu(self.conv1_in(self.conv1(x)), negative_slope=0.2)
-            residual1 = x
-            x = F.leaky_relu(self.conv2_in(self.conv2(x)), negative_slope=0.2)
-            residual2 = x
-            x = F.leaky_relu(self.conv3_in(self.conv3(x)), negative_slope=0.2)
-            residual3 = x
-            x = F.leaky_relu(self.conv4_in(self.conv4(x)), negative_slope=0.2)
-            residual4 = x
-            x = F.leaky_relu(self.conv5_in(self.conv5(x)), negative_slope=0.2)
-            # Decoder
-            x = F.leaky_relu(self.deconv1_in(self.deconv1(x)), negative_slope=0.2)
-            x = F.leaky_relu(self.deconv2_in(self.deconv2(torch.cat([x, residual4], dim=1))), negative_slope=0.2)
-            x = F.leaky_relu(self.deconv3_in(self.deconv3(torch.cat([x, residual3], dim=1))), negative_slope=0.2)
-            x = F.leaky_relu(self.deconv4_in(self.deconv4(torch.cat([x, residual2], dim=1))), negative_slope=0.2)
-            x = F.tanh(self.deconv5_in(self.deconv5(torch.cat([x, residual1], dim=1))))
-
-            return x
-
     device = torch.device('cuda' if torch.cuda.is_available() and args.useGPU else 'cpu')
     print("Code is running on " + str(device))
     texture_features1 = torch.randn(1, 16, 64, 64, requires_grad=True, device=device)
@@ -90,15 +38,15 @@ if __name__ == '__main__':
     # from torch.autograd import Variable
     # texture_features = Variable(torch.randn(1, 16, 512, 512), requires_grad=True, device=device)
     renderer = Renderer(use_shading=False, device=device)
-    model = Net()
+    model = UNet(n_channels=16, n_classes=3, bilinear=False)
     model = model.to(device)
     texture_features1 = texture_features1.to(device)
     texture_features2 = texture_features2.to(device)
     texture_features3 = texture_features3.to(device)
     texture_features4 = texture_features4.to(device)
-    # # check keras-like model summary using torchsummary
-    # from torchsummary import summary
-    # summary(model, texture_features.shape)
+    # check keras-like model summary using torchsummary
+    from torchsummary import summary
+    summary(model, (16, 512, 1024))
     loader = FrameBufferLoader(path=args.trainset)
     testloader = FrameBufferLoader(path=args.testset)
     print("Loading data is done!")
